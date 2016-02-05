9481ede7a5843e4b2ed4394c672e76fb5c1bab00
Maxime <maximeloizeau@gmail.com>
2016-02-05 12:50:24 +0100

    Add user and date to message object before broadcasting it to users

--- controllers/socket_message.js
+++ controllers/socket_message.js
@@ -8,13 +8,11 @@ const User = require('../models/user');
 const ChatRoom = require('../models/chatroom');
 
 function setup(endpoint, socket) {
-    console.log("Client connected");
     socket.on('message', messageReceived.bind(null, endpoint, socket));
     socket.on('join', joiningRoom.bind(null, endpoint, socket));
 }
 
 function messageReceived(endpoint, socket, message) {
-    console.log(message);
     if(!message || !message.token || !message.text) {
         // TODO return error message to user
         return;
@@ -23,10 +21,9 @@ function messageReceived(endpoint, socket, message) {
     auth.loggedUser(message.token, processMessage);
 
     function processMessage(userLoggedIn, user) {
-        console.log("Message processed", userLoggedIn, message);
         if(!userLoggedIn) return;
 
-        endpoint.in(message.room).emit('message', { text: message.text });
+        endpoint.in(message.room).emit('message', { text: message.text, user: user.username, date: new Date() });
     }
 }
 
